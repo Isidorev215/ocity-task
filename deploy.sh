16c731f8df799e8402d0f6d6cbfc83ca9fbe48de
Nwaiwu Isidore <nwaiwuisidore@gmail.com>
2021-09-30 19:59:53 +0100

    Added github-pages serving files

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+cd dist
+
+git init
+git add -add
+git commit -m 'New Deploy'
+git push -f git@github.com:Isidorev215/ocity-task.git master:gh-pages
+
+cd -
